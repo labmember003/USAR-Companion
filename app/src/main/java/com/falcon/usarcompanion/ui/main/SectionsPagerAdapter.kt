4c9ebe351ad02bf088c0a0bc9159dd0623141c79
Avishisht <avishishtgupta@gmail.com>
2022-09-02 23:37:09 +0530

    Added forced light theme

--- app/src/main/java/com/falcon/usarcompanion/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/falcon/usarcompanion/ui/main/SectionsPagerAdapter.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.falcon.usarcompanion.R
+import com.falcon.usarcompanion.overview.OverviewFragment
 
 private val TAB_TITLES = arrayOf(
     R.string.tab_text_1,
@@ -23,7 +24,7 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     override fun getItem(position: Int): Fragment {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1)
+        return OverviewFragment()
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
