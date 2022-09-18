4d106269e7745c848ae2d60699af4048c756a0c9
Avishisht <avishishtgupta@gmail.com>
2022-09-18 14:26:22 +0530

    added navigation drawer icons

--- app/src/main/java/com/falcon/usarcompanion/Resources.kt
+++ app/src/main/java/com/falcon/usarcompanion/Resources.kt
@@ -9,16 +9,6 @@ import androidx.navigation.fragment.findNavController
 import com.falcon.usarcompanion.databinding.FragmentFirstBinding
 import com.falcon.usarcompanion.databinding.FragmentResourcesBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Resources.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Resources : Fragment() {
 
     private var _binding: FragmentResourcesBinding? = null
