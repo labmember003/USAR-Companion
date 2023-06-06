5ed49842ac93d1bda9f64b2cbb93fb3a7931a2dc
labmember003 <93467641+labmember003@users.noreply.github.com>
2023-06-06 14:23:18 +0530

    added timeout

--- app/src/main/java/com/falcon/usarcompanion/network/ApiService.kt
+++ app/src/main/java/com/falcon/usarcompanion/network/ApiService.kt
@@ -2,21 +2,30 @@ package com.falcon.usarcompanion.network
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Url
+import java.util.concurrent.TimeUnit
+
 
 private const val BASE_URL = "https://github.com/labmember003/usar_data/raw/master/"
 
 private val moshi = Moshi.Builder().build()
 
+var okHttpClient = OkHttpClient().newBuilder()
+    .connectTimeout(60, TimeUnit.SECONDS)
+    .readTimeout(60, TimeUnit.SECONDS)
+    .writeTimeout(60, TimeUnit.SECONDS)
+    .build()
+
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .addCallAdapterFactory(CoroutineCallAdapterFactory())
     .baseUrl(BASE_URL)
+    .client(okHttpClient)
     .build()
 
 interface YearDataApiService {
